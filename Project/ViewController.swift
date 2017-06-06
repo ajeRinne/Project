cc6ff13e28b7b3bf0b2af691504541d8ef3db391
Alexander Rinne <Alex@wcw-stud-145-109-7-139.wireless.uva.nl>
2017-06-06 17:08:21 +0200

    Initial Commit

---
+++ Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Alexander Rinne on 06-06-17.
+//  Copyright © 2017 Alexander Rinne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
